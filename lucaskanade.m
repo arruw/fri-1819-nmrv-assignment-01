ddd3ce06a759edd5f1157cbbef3f00968483a061
Matjaž Mav <matjaz.mav@gmail.com>
2019-03-08 09:25:17 +0100

    Implemented Lucas-Kanade

--- lucaskanade.m
+++ lucaskanade.m
@@ -1,2 +1,28 @@
 function [U, V] = lucaskanade(I1, I2, n)
 
+[I1x, I1y] = gaussderiv(I1, 1);
+[I2x, I2y] = gaussderiv(I2, 1);
+Ix = 1/2 .* (I1x + I2x);
+Iy = 1/2 .* (I1y + I2y);
+It = gausssmooth(I2 - I1, 1.0);
+
+Ixt = Ix.*It;
+Iyt = Iy.*It;
+Ix2 = Ix.^2;
+Iy2 = Iy.^2;
+Ixy = Ix.*Iy;
+
+u = ones(3);
+D = conv2(Ix2, u, 'same') .* \
+    conv2(Iy2, u, 'same') .- \
+    conv2(Ixy, u, 'same').^2;
+
+U = (((conv2(Iy2, u, 'same') .* \
+    conv2(Ixt, u, 'same')) .- \
+    (conv2(Ixy, u, 'same') .* \
+    conv2(Iyt, u, 'same'))) ./ D) .* -1;
+
+V = (((conv2(Ix2, u, 'same') .* \
+    conv2(Iyt, u, 'same')) .- \
+    (conv2(Ixy, u, 'same') .* \
+    conv2(Ixt, u, 'same'))) ./ D) .* -1;
--- solution.m
+++ solution.m
@@ -3,10 +3,17 @@ pkg load image;
 I1 = rand(200, 200);
 I2 = imrotate(I1, 1, 'bicubic', 'crop');
 
+%I1 = imread('disparity/cporta_left.png');
+%I2 = imread('disparity/cporta_right.png');
+
+[Ulk, Vlk] = lucaskanade(I1, I2, 3);
+
 figure(1); clf;
 hold on;
-subplot(3, 2, 1); imshow(I1); title('Image 1');
-subplot(3, 2, 2); imshow(I2); title('Image 2');
+subplot(2, 2, 1); imshow(I1); title('Image 1');
+subplot(2, 2, 2); imshow(I2); title('Image 2');
+subplot(2, 2, 3); showflow(Ulk, Vlk, 'color'); title('Lucas-Kanade (color)');
+subplot(2, 2, 4); showflow(Ulk, Vlk, 'field'); title('Lucas-Kanade (field)');
 
 waitfor(gcf);
 
